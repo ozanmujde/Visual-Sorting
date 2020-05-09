77958da4876717d7d2e0c9616846358a4957934b
ozanmujde <ozanmujde@googlemail.com>
2020-05-09 20:03:57 +0300

    Finish The Project
    I cannot implement background color with setOpaque it gives problem
    so i m not going to use background color.I m not going to use speed
    or size sliders because they are not looking good

--- Printer.java
+++ Printer.java
@@ -7,13 +7,13 @@ import java.util.Arrays;
 /*
    TO DO:
     0) Make current in different color ++
-    1) Add Different sorting algorithms and make it work with different algos
+    1) Add Different sorting algorithms and make it work with different algos ++
     1.5) Add something like chooser and work the chooser in other method. not in the Constructor++
     2) Add a Start to Menu ++
     3) Make a Menu to Choose algo++
     4) Add speed chooser to menu to 0 to 1
     5) Make this Menu preface like login screen ++
-    6) Add restart After Sorting is finish
+    6) Add restart After Sorting is finish++
  */
 public class Printer extends JComponent implements ActionListener {
     private final int WIDTH = 1280;
@@ -24,12 +24,14 @@ public class Printer extends JComponent implements ActionListener {
     private JFrame mainFrame;
     private JButton starter;
     private JButton restart;
+    private JPanel background;
     private JComboBox<String> algorithms;
 
     public Printer() {
         //Set the items//////
         algorithms = new JComboBox<>();
         mainFrame = new JFrame();
+        background = new JPanel();
         starter = new JButton("Start");
         restart = new JButton("Restart");
         restart.addActionListener(this);
@@ -37,8 +39,7 @@ public class Printer extends JComponent implements ActionListener {
         starter.setBounds(10, 100, 100, 50);
         algorithms.setBounds(10, 40, 100, 50);
         restart.setBounds(540, 300, 100, 60);
-        setOpaque(true);
-        mainFrame.setBackground(Color.GRAY);
+
         ///////////////////////
         // Add algorithms //
         algorithms.addItem("Bubble Sort");
@@ -61,7 +62,6 @@ public class Printer extends JComponent implements ActionListener {
         restart.setVisible(false);
         mainFrame.add(this);
         mainFrame.setVisible(true);
-        // bubbleSort(data);
     }
 
     @Override
@@ -74,8 +74,7 @@ public class Printer extends JComponent implements ActionListener {
             else
                 g2.setColor(Color.GRAY);
             if (count == 0) {
-
-                g2.setColor(Color.WHITE);// starter screen will be look different
+                g2.setColor(Color.RED);// starter screen will be look different
             }
             g2.drawLine(i, HEIGHT, i, HEIGHT - data[i]);
         }
@@ -104,17 +103,18 @@ public class Printer extends JComponent implements ActionListener {
                 insertionSort(data);
                 break;
             case "Merge Sort":
-                mergeSort(data,0,data.length-1);
+                mergeSort(data, 0, data.length - 1);
                 break;
             case "Radix Sort":
-                radixSort(data,data.length);
+                radixSort(data, data.length);
                 break;
             case "Cocktail Sort":
                 cocktailSort(data);
                 break;
 
         }
-        cur = -1;//delete the current pointer
+        cur = -1;//delete the current pointer to dont point orange
+        System.out.println(count);
         repaint();
         restart.setVisible(true);
     }
@@ -179,6 +179,7 @@ public class Printer extends JComponent implements ActionListener {
             start = start + 1;
         }
     }
+
     ////////////////////////
     private boolean isSwapped(int[] a, boolean swapped, int i) {
         if (a[i] > a[i + 1]) {
@@ -195,21 +196,19 @@ public class Printer extends JComponent implements ActionListener {
 
     //////////////////////////
     /////////////Merge Sort////
-    private void merge(int arr[], int l, int m, int r)
-    {
+    private void merge(int[] arr, int l, int m, int r) {
         // Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
 
         /* Create temp arrays */
-        int L[] = new int [n1];
-        int R[] = new int [n2];
+        int[] L = new int[n1];
+        int[] R = new int[n2];
 
         /*Copy data to temp arrays*/
-        for (int i=0; i<n1; ++i)
-            L[i] = arr[l + i];
-        for (int j=0; j<n2; ++j)
-            R[j] = arr[m + 1+ j];
+        if (n1 >= 0) System.arraycopy(arr, l, L, 0, n1);
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
 
 
         /* Merge the temp arrays */
@@ -219,18 +218,14 @@ public class Printer extends JComponent implements ActionListener {
 
         // Initial index of merged subarry array
         int k = l;
-        while (i < n1 && j < n2)
-        {
-            if (L[i] <= R[j])
-            {
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
                 arr[k] = L[i];
                 cur = k;
                 repaint();
                 delay(1_500_000);
                 i++;
-            }
-            else
-            {
+            } else {
                 cur = k;
                 repaint();
                 delay(1_500_000);
@@ -241,8 +236,7 @@ public class Printer extends JComponent implements ActionListener {
         }
 
         /* Copy remaining elements of L[] if any */
-        while (i < n1)
-        {
+        while (i < n1) {
             arr[k] = L[i];
             cur = k;
             repaint();
@@ -252,8 +246,7 @@ public class Printer extends JComponent implements ActionListener {
         }
 
         /* Copy remaining elements of R[] if any */
-        while (j < n2)
-        {
+        while (j < n2) {
             arr[k] = R[j];
             j++;
             k++;
@@ -262,24 +255,23 @@ public class Printer extends JComponent implements ActionListener {
 
     // Main function that sorts arr[l..r] using
     // merge()
-    private void mergeSort(int arr[], int l, int r)
-    {
-        if (l < r)
-        {
+    private void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
             // Find the middle point
-            int m = (l+r)/2;
+            int m = (l + r) / 2;
 
             // Sort first and second halves
             cur = m;
             repaint();
             delay(3_000_000);
             mergeSort(arr, l, m);
-            mergeSort(arr , m+1, r);
+            mergeSort(arr, m + 1, r);
 
             // Merge the sorted halves
             merge(arr, l, m, r);
         }
     }
+
     //////////////////////////
     ////Insertion Sort/////////
     private void insertionSort(int[] arr) {
@@ -305,7 +297,7 @@ public class Printer extends JComponent implements ActionListener {
 
     ///////////////////////
     ///////////Quick Sort////
-    private int partition(int arr[], int low, int high) {
+    private int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
         int i = (low - 1); // index of smaller element
         for (int j = low; j < high; j++) {
@@ -353,9 +345,9 @@ public class Printer extends JComponent implements ActionListener {
             quickSort(arr, pi + 1, high);
         }
     }
+
     //////////////////Heap Sort///////////
-    public void heapSort(int arr[])
-    {
+    public void heapSort(int[] arr) {
         int n = arr.length;
 
         // Build heap (rearrange array)
@@ -363,8 +355,7 @@ public class Printer extends JComponent implements ActionListener {
             heapify(arr, n, i);
 
         // One by one extract an element from heap
-        for (int i=n-1; i>0; i--)
-        {
+        for (int i = n - 1; i > 0; i--) {
             // Move current root to end
             int temp = arr[0];
             arr[0] = arr[i];
@@ -379,11 +370,10 @@ public class Printer extends JComponent implements ActionListener {
 
     // To heapify a subtree rooted with node i which is
     // an index in arr[]. n is size of heap
-    private void heapify(int []arr, int n, int i)
-    {
+    private void heapify(int[] arr, int n, int i) {
         int largest = i; // Initialize largest as root
-        int l = 2*i + 1; // left = 2*i + 1
-        int r = 2*i + 2; // right = 2*i + 2
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
 
         // If left child is larger than root
         if (l < n && arr[l] > arr[largest])
@@ -394,8 +384,7 @@ public class Printer extends JComponent implements ActionListener {
             largest = r;
 
         // If largest is not root
-        if (largest != i)
-        {
+        if (largest != i) {
             int swap = arr[i];
             arr[i] = arr[largest];
             arr[largest] = swap;
@@ -406,10 +395,10 @@ public class Printer extends JComponent implements ActionListener {
             heapify(arr, n, largest);
         }
     }
+
     /////////////////////////////////////
     /////////////Radix Sort/////////////
-    private int getMax(int arr[], int n)
-    {
+    private int getMax(int[] arr, int n) {
         int mx = arr[0];
         for (int i = 1; i < n; i++)
             if (arr[i] > mx)
@@ -419,27 +408,25 @@ public class Printer extends JComponent implements ActionListener {
 
     // A function to do counting sort of arr[] according to
     // the digit represented by exp.
-    private void countSort(int arr[], int n, int exp)
-    {
-        int output[] = new int[n]; // output array
+    private void countSort(int[] arr, int n, int exp) {
+        int[] output = new int[n]; // output array
         int i;
-        int count[] = new int[10];
-        Arrays.fill(count,0);
+        int[] countarr = new int[10];
+        Arrays.fill(countarr, 0);
 
-        // Store count of occurrences in count[]
+        // Store count of occurrences in countarr[]
         for (i = 0; i < n; i++)
-            count[ (arr[i]/exp)%10 ]++;
+            countarr[(arr[i] / exp) % 10]++;
 
-        // Change count[i] so that count[i] now contains
+        // Change countarr[i] so that countarr[i] now contains
         // actual position of this digit in output[]
         for (i = 1; i < 10; i++)
-            count[i] += count[i - 1];
+            countarr[i] += countarr[i - 1];
 
         // Build the output array
-        for (i = n - 1; i >= 0; i--)
-        {
-            output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
-            count[ (arr[i]/exp)%10 ]--;
+        for (i = n - 1; i >= 0; i--) {
+            output[countarr[(arr[i] / exp) % 10] - 1] = arr[i];
+            countarr[(arr[i] / exp) % 10]--;
         }
 
         // Copy the output array to arr[], so that arr[] now
@@ -454,15 +441,14 @@ public class Printer extends JComponent implements ActionListener {
 
     // The main function to that sorts arr[] of size n using
     // Radix Sort
-    private void radixSort(int [] arr, int n)
-    {
+    private void radixSort(int[] arr, int n) {
         // Find the maximum number to know number of digits
         int m = getMax(arr, n);
 
         // Do counting sort for every digit. Note that instead
         // of passing digit number, exp is passed. exp is 10^i
         // where i is current digit number
-        for (int exp = 1; m/exp > 0; exp *= 10)
+        for (int exp = 1; m / exp > 0; exp *= 10)
             countSort(arr, n, exp);
     }
     ////////////////////////////////////
@@ -479,7 +465,6 @@ public class Printer extends JComponent implements ActionListener {
         String command = e.getActionCommand();
         if (command.equals("Start") && count == 0) {
             restart.setVisible(false);
-            setOpaque(false);
             starter.setVisible(false);// i dont want to see after i click
             algorithms.setVisible(false);
             count++;
@@ -496,7 +481,6 @@ public class Printer extends JComponent implements ActionListener {
             restart.setVisible(false);
             starter.setVisible(true);
             algorithms.setVisible(true);
-            mainFrame.remove(restart);
             count = 0;
             randomize(data);
             repaint();
